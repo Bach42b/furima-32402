63f54ea0681fe69035e56efc80399c548dd06188
Bach42b <juli.a.julia.328.723@gmail.com>
2021-03-28 21:19:55 +0900

    デプロイ

---
+++ test/controllers/flea_markets_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class FleaMarketsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get flea_markets_index_url
+    assert_response :success
+  end
+
+end
