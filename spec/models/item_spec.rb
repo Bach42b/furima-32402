9fedb830783338b25be3bf7becbc3aa02e32c852
Bach42b <juli.a.julia.328.723@gmail.com>
2021-04-30 00:42:45 +0900

    モデル単体テスト修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,85 +5,78 @@ RSpec.describe Item, type: :model do
     @item = FactoryBot.build(:item)
   end
 
-  describe '新規商品登録' do
-    context '新規商品登録がうまくいくとき' do
-      it '全ての記述が正しく入力されれば登録できる' do
-        expect(@item).to be_valid
-      end
+
+  
+    it '出品画像が空では登録できないこと' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
     end
 
-    context '新規商品登録がうまくいかないとき' do
-      it '出品画像が空では登録できないこと' do
-        @item.image = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Image can't be blank")
-      end
-
-      it '商品名が空では登録できないこと' do
-        @item.product_name = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Product name can't be blank")
-      end
-
-      it '商品説明が空では登録できないこと' do
-        @item.description = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Description can't be blank")
-      end
-
-      it 'カテゴリーが空では登録できないこと' do
-        @item.category_id = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Category is not included in the list")
-      end
-
-      it '商品の状態が空では登録できないこと' do
-        @item.condition_id = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Condition is not included in the list")
-      end
-
-      it '配送料の負担が空では登録できないこと' do
-        @item.delivery_fee_id = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery fee is not included in the list")
-      end
-
-      it '発送元の地域が空では登録できないこと' do
-        @item.area_id = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Area is not a number")
-      end
-
-      it '発送までの日数が空では登録できないこと' do
-        @item.delivery_time_id = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery time is not included in the list")
-      end
-
-      it '販売価格が空だと登録できないこと' do
-        @item.price = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
-      end
-
-      it '価格が¥300以下だと登録できないこと' do
-        @item.price = '299'
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not included in the list")
-      end
-
-      it '価格が¥10000000以上だと登録できないこと' do
-        @item.price = '10000000'
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not included in the list")
-      end
-
-      it '販売価格は半角数字でないと登録できないこと' do
-        @item.price = 'あ'
-        @item.valid?
-        expect(@item.errors.full_messages).to include('Price is not a number')
-      end
+    it '商品名が空では登録できないこと' do
+      @item.product_name = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Product name can't be blank")
     end
-  end
+
+    it '商品説明が空では登録できないこと' do
+      @item.description = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
+    end
+
+    it 'カテゴリーが空では登録できないこと' do
+      @item.category_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category is not included in the list")
+    end
+
+    it '商品の状態が空では登録できないこと' do
+      @item.condition_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition is not included in the list")
+    end
+
+    it '配送料の負担が空では登録できないこと' do
+      @item.delivery_fee_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery fee is not included in the list")
+    end
+
+    it '発送元の地域が空では登録できないこと' do
+      @item.area_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Area is not a number")
+    end
+
+    it '発送までの日数が空では登録できないこと' do
+      @item.delivery_time_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery time is not included in the list")
+    end
+
+    it '販売価格が空だと登録できないこと' do
+      @item.price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it '価格が¥300以下だと登録できないこと' do
+      @item.price = '299'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not included in the list")
+    end
+
+    it '価格が¥10000000以上だと登録できないこと' do
+      @item.price = '10000000'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not included in the list")
+    end
+
+    it '販売価格は半角数字でないと登録できないこと' do
+      @item.price = 'あ'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price is not a number')
+    end
+  
 end
